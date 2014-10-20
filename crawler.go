11d57b63fcf2b4f0e59ff5e4de73666887f1a0da
Matt DuVall <matt@stripe.com>
2014-10-19 17:05:44 -0700

    Retry failed sites & stats

--- crawler.go
+++ crawler.go
@@ -8,14 +8,16 @@ import (
 )
 
 const NUM_WORKERS = 20
-const NUM_SITES = 1000
+const NUM_SITES = 100
+
+var failedSites []string
 
 func crawlerCmd(site string) (string, string, string) {
 	return "node", "crawler.js", site
 }
 
 func slurpSitesFile() string {
-	siteContents, err := ioutil.ReadFile("sites-1000.txt")
+	siteContents, err := ioutil.ReadFile("sites-100.txt")
 	if err != nil {
 		panic("The file (sites.txt) isn't even here, we're done.")
 	}
@@ -32,12 +34,15 @@ func pwd() string {
 	return string(out)
 }
 
-func work(siteJobs <-chan string, out chan<- string) {
+func work(siteJobs <-chan string, res chan<- string) {
 	for site := range siteJobs {
 		out, err := exec.Command(crawlerCmd(site)).Output()
+		res <- "OK"
+
 		if err != nil {
 			fmt.Println("---> Crawl of site:", site, "failed!")
 			fmt.Println("\t", err)
+			failedSites = append(failedSites, site)
 			continue
 		}
 
@@ -51,24 +56,58 @@ func crawlSites(sites string) {
 
 	siteJobs := make(chan string, NUM_SITES)
 	out := make(chan string, NUM_SITES)
+	failedSites = make([]string, 0)
 
+	// Kick off worker pool
 	for i := 1; i <= NUM_WORKERS; i++ {
 		go work(siteJobs, out)
 	}
 
+	totalSites := 0
 	for _, line := range strings.Split(sites, "\n") {
 		splitLine := strings.Split(line, ",")
 		// Could hit \n at the end, too lazy to fix right now
 		if len(splitLine) == 2 {
 			_, site := splitLine[0], splitLine[1]
 			siteJobs <- site
+			totalSites++
 		}
 	}
+
 	close(siteJobs)
 
-	for i := 0; i <= NUM_WORKERS; i++ {
-		fmt.Println(<-out)
+	// Wait for all the jobs to flush
+	for i := 0; i < NUM_SITES; i++ {
+		<-out
 	}
+
+	numFailedSites := len(failedSites)
+	retryJobs := make(chan string, numFailedSites)
+
+	// Kick off retry worker pool
+	for i := 1; i <= NUM_WORKERS; i++ {
+		go work(retryJobs, out)
+	}
+
+	// Then push failed sites back on channels
+	for _, failedSite := range failedSites {
+		fmt.Println("<=== Requeueing job for failed site", failedSite)
+		retryJobs <- failedSite
+	}
+
+	close(retryJobs)
+
+	// Retry the jobs and wait
+	fmt.Println("This many sites failed:", numFailedSites)
+	for i := 0; i < (numFailedSites - 1); i++ {
+		<-out
+	}
+
+	fmt.Println("========== WE ARE DONE!!!! ==========")
+	fmt.Println("Here are some statistics!")
+	fmt.Println("\t# Total sites in set:", totalSites)
+	fmt.Println("\t# Failed sites:", numFailedSites)
+	fmt.Println("\tFailed sites:", failedSites)
 }
 
 func main() {
