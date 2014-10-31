ddf287d0c11ed4b8a56662282de6d25213ddde21
Thomas Davis <thomasalwyndavis@gmail.com>
2014-10-31 17:00:11 +1000

    store 1million

--- buildQueue.js
+++ buildQueue.js
@@ -4,14 +4,16 @@ var fs = require('fs');
 var kue = require('kue')
   , jobs = kue.createQueue();
 
-var data = fs.readFileSync('sites-1000.txt', 'utf8');
+var data = fs.readFileSync('sites.txt', 'utf8');
 
 var lines = data.split("\n");
 
 for (var i = 0; i < lines.length; i++) {
   if (/,/.test(lines[i])) {         
     var lineData = lines[i].split(",");
-    jobs.create('website', { url: lineData[1], rank: lineData[0] }).attempts(3).save();;
+    jobs.create('website', { url: lineData[1], rank: lineData[0] }).attempts(10).backoff( {delay: 60*1000, type:'fixed'} ).save(function(){
+      console.log(i);
+    });;
   }
 }
 
