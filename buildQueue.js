86a0974765e1ae4192ab04e5a7b6df1626ac4ff5
Thomas Davis <thomasalwyndavis@gmail.com>
2014-10-31 17:15:00 +1000

    async

--- buildQueue.js
+++ buildQueue.js
@@ -7,18 +7,27 @@ var kue = require('kue')
 var data = fs.readFileSync('sites.txt', 'utf8');
 
 var lines = data.split("\n");
-
-for (var i = 0; i < lines.length; i++) {
-  if (/,/.test(lines[i])) {         
-    var lineData = lines[i].split(",");
+var a = 0;
+async.eachLimit(lines, 10, function (line, done) {
+  a++;
+  if (/,/.test(line)) {         
+    var lineData = line.split(",");
     jobs.create('website', { url: lineData[1], rank: lineData[0] }).attempts(10).backoff( {delay: 60*1000, type:'fixed'} ).save(function(){
-      console.log(i);
+      console.log(a);
+      done();
+
     });;
+  } else {
+    done();
   }
+}, function(err){
+    // if any of the saves produced an error, err would equal that error
+    var totalSites = lines.length;
+console.log(totalSites)
+});
 }
 
-var totalSites = lines.length;
-console.log(totalSites)
+
 
 //websiteQueue.process(function(job, done){
 
