d55291995d607be45f8bfd161c1784f910245f9c
Jason Chen <jhchen7@gmail.com>
2015-06-28 18:41:23 -0700

    remove unnecessary code

--- runner.js
+++ runner.js
@@ -13,9 +13,7 @@ var cluster = require('cluster');
 *****************/
 
 var jobs = kue.createQueue({
-	redis: {
-		host: 'crawl-queue'
-	}
+	redis: { host: 'crawl-queue' }
 });
 var clusterWorkerSize = require('os').cpus().length;
 
@@ -64,34 +62,6 @@ function out (success, stage, message) {
      Main
 *************/
 
-function processSitesFile () {
-	FS.readFile(Arguments.sitesFile, { encoding: "utf-8" }, function (error, data) {
-		if (error) {
-			out(false, "Could not read sites file", Arguments.sitesFile);
-		} else {
-			out(true, "Read sites file", Arguments.sitesFile);
-
-			var lines = data.split("\n");
-
-			for (var i = 0; i < lines.length; i++) {
-				if (/,/.test(lines[i])) {
-					var lineData = lines[i].split(",");
-
-					sites.push({ url: lineData[1], rank: lineData[0] });
-				}
-			}
-
-			if (Arguments.siteOffsetStart) {
-				sites = sites.slice(Arguments.siteOffsetStart - 1, Arguments.siteOffsetEnd);
-			}
-
-			totalSites = sites.length;
-
-			spawnCrawls(sites);
-		}
-	});
-}
-
 function crawl (site, callback) {
 	var spawn = require("child_process").spawn;
 	var crawler = spawn("node", [ "crawler.js", site.url, site.rank ]);
@@ -130,17 +100,12 @@ function crawl (site, callback) {
 	});
 }
 
-function spawnCrawls (sites) {
+function spawnCrawls () {
 	function reportCrawlsDone() {
 		out(true, "Failed sites", failedSites || "none");
 		out(true, "Stats", (totalSites - failedSites.length) + "/" + totalSites + " sites succeeded");
 		out(true, "Done");
 	}
-	/*
-	if (Arguments.siteOffset) {
-		sites = sites.slice(Arguments.siteOffset - 1);
-	}
-	*/
 	if (cluster.isMaster) {
 	  for (var i = 0; i < clusterWorkerSize; i++) {
 	    cluster.fork();
@@ -148,29 +113,8 @@ function spawnCrawls (sites) {
 	} else {
 
 		jobs.process('website', 3, function(job, done){
-		//Async.eachLimit(sites, Arguments.concurrency, crawl, function(error) {
 			console.log(job.data);
 			crawl(job.data, done);
-			/*
-			if (error) {
-				throw new Error(error);
-			}
-
-			if (failedSites.length) {
-				var retryConcurrency = Math.floor(Arguments.concurrency * 0.75);
-
-				out(true, "Failed sites on first try", failedSites.length);
-				out(true, "Re-crawling failed sites with " + retryConcurrency + " concurrency...");
-
-				sites = failedSites.slice(0);
-				failedSites = [];
-
-				Async.eachLimit(sites, retryConcurrency, crawl, function() {
-					reportCrawlsDone();
-				});
-			} else {
-				reportCrawlsDone();
-			}*/
 		});
 	}
 
@@ -181,4 +125,4 @@ function spawnCrawls (sites) {
 ****************/
 
 out(true, "Running");
-processSitesFile();
+spawnCrawls();
