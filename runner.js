d48024e58b9246ead79e7f7ddc3670ea6e6891e9
Jason Chen <jhchen7@gmail.com>
2015-07-06 22:33:20 -0700

    log job id an kill with -9

--- runner.js
+++ runner.js
@@ -14,37 +14,36 @@ var jobs = kue.createQueue({
   }
 });
 jobs.process('website', CONCURRENCY, function(job, done) {
-  crawl(job.data, done);
+  crawl(job, done);
 });
 
 setTimeout(function() {
   jobs.shutdown(CRAWL_TIMEOUT, function(err) {
     process.exit(0);
   });
-  dumpFile.unwatch();
 }, 60*60*1000);  // Kill ourselves after an hour to prevent long running process issues
 
 
-function crawl(site, callback) {
-  var crawler = child_process.spawn("node", [ "crawler.js", site.domain, site.id ]);
+function crawl(job, callback) {
+  var crawler = child_process.spawn("node", [ "crawler.js", job.data.domain, job.data.id ]);
 
   var killTimer = setTimeout(function() {
-    crawler.kill();
+    crawler.kill('SIGKILL');
   }, CRAWL_TIMEOUT);
 
   crawler.on("close", function (code) {
     clearTimeout(killTimer);
     switch (code) {
       case 0:
-        console.log("Success " + site.domain);
+        console.log("Success " + job.data.domain, job.id);
         callback();
         break;
       case 1:
-        console.log("Failed " + site.domain);
+        console.log("Failed " + job.data.domain, job.id);
         callback('Crawl failed');
         break;
       case null:
-        console.log("Killed " + site.domain);
+        console.log("Killed " + job.data.domain, job.id);
         callback('Crawl failed - killed');
         break;
     }
